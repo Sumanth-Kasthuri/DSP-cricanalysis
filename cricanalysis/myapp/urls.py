0a0605a0d34023dfdbb108335241d130789f4137
sumanth kasthuri <sumanth@sumanths-MacBook-Pro.local>
2025-04-12 12:42:00 +0100

    basic templates and user auth

---
+++ cricanalysis/myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    # Add more URL patterns as needed
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
+    path('matches/', views.matches_view, name='matches'),
+    path('teams/', views.teams_views, name='teams'),
+    path('players/', views.players_view, name='players'),
+    path('profile/', views.profile_view, name='profile'),
+]
